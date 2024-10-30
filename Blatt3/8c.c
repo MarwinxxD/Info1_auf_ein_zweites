51ea841d92f32ee506577ea901d9fd2161bdc44c
Blacky010 <154694345+Blacky010@users.noreply.github.com>
2024-10-30 13:41:19 +0100

    8 finaly

--- Blatt3/8c.c
+++ Blatt3/8c.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
 int string_index_of_sequence(char v[], char find[]);
 
 int main(void)
 {
+        char s[] = "Schokolade";
+        char t[] = "oko";
+        char u[] = "Sch";
+        char v[] = "ade";
+        char w[] = "x@z";
+        char x[] = "";
+        char y[] = "k";
+        char z[] = "o";
 
+        int index = string_index_of_sequence(s, t);
+        printf("Test 1: Start index of %s in Schokolade: %i (Loesung: 3)\n", t, index);
+
+        index = string_index_of_sequence(s, u);
+        printf("Test 2: Start index of %s in Schokolade: %i (Loesung: 0)\n", u, index);
+    
+        index = string_index_of_sequence(s, v);
+        printf("Test 3: Start index of %s in Schokolade: %i (Loesung: 7)\n", v, index);
+
+        index = string_index_of_sequence(s, w);
+        printf("Test 4: Start index of %s in Schokolade: %i (Loesung: -1)\n", w, index);
+
+        index = string_index_of_sequence(s, x);
+        printf("Test 5: Start index of %s in Schokolade: %i (Loesung: -1)\n", x, index);
+
+        index = string_index_of_sequence(s, y);
+        printf("Test 6: Start index of %s in Schokolade: %i (Loesung: 4)\n", y, index);
+
+        index = string_index_of_sequence(s, z);
+        printf("Test 7: Start index of %s in Schokolade: %i (Loesung: 3)\n", z, index);
+    
         return 0;
 }
 
@@ -17,6 +47,10 @@ int string_index_of_sequence(char v[], char find[])
         if (len_find > len_v) {
                 return -1;
         }
+        if(len_find == 0) {
+                return -1;
+        }
+
 
         for (int i = 0; i <= len_v - len_find; i++) {
                 for (j = 0; j < len_find; j++) {
---
+++ Blatt3/string_tolower_angabe.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void string_tolower(char v[]);
+
+int main(void)
+{
+    char s[] = "SchOkoLaDE";
+    char t[] = "SCHOKOLADE";
+    char u[] = "schokolade";
+    char v[] = "";
+    char w[] = "Ch0c0!@#LATE";
+
+    string_tolower(s);
+    printf("Test 1: %s (schokolade)\n", s);
+
+    string_tolower(t);
+    printf("Test 2: %s (schokolade)\n", t);
+
+    string_tolower(u);
+    printf("Test 3: %s (schokolade)\n", u);
+
+    string_tolower(v);
+    printf("Test 4: \"%s\" (\"\")\n", v);
+
+    string_tolower(w);
+    printf("Test 5: %s (ch0c0!@#late)\n", w);
+
+    return 0;
+}
+
+void string_tolower(char v[]){
+	/*Fügen Sie hier den Code ein.*/    
+}
