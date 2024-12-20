07bc1251a2a9e982714616b1b898c1c74dce0816
Eri17 <78800800+Eri17@users.noreply.github.com>
2024-12-20 22:51:57 +0100

    anfang aufg34

---
+++ Blatt10/src/34.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int increment_if_zero(int *x, int *y){
+        if(y == 0){
+                x++;
+                return 1;
+        }
+        return 0;
+}
+
+int main(void){
+ return 0;       
+}
